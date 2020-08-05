aa4ef67dd7041985d168075039d45f8e232cf566
Lee <38075314+MarkHooland@users.noreply.github.com>
2020-08-05 11:03:16 +0800

    update

--- C++/Class/main.cpp
+++ C++/Class/main.cpp
@@ -41,5 +41,23 @@ double Rectangle::getArea() const
 }
 int main()
 {
+  Rectangle Box;
+  double rectWidth;
+  double rectLength;
+
+  cout << "This program will calculate the area of a rectangle." << endl;
+  cout << "What is the width? ";
+  cin >> rectWidth;
+  cout << "What is the length? ";
+  cin >> rectLength;
+
+  Box.setWidth(rectWidth);
+  Box.setLength(rectLength);
+
+  cout << "Here is the rectangle's data: " << endl;
+  cout << "Width: " << Box.getWidth() << endl;
+  cout << "Length: " << Box.getLength() << endl;
+  cout << "Area: " << Box.getArea() << endl;
+
   return 0;
 }
