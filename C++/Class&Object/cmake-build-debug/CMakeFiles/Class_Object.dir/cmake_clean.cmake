f25ab049440d76afa4dd3f70b3fe8838a71a3fe2
TheRealMilesLee <1789216340lhy@gmail.com>
2020-08-05 09:38:23 +0800

    update

---
+++ C++/Class&Object/cmake-build-debug/CMakeFiles/Class_Object.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Class_Object.dir/main.cpp.o"
+  "Class_Object"
+  "Class_Object.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Class_Object.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
