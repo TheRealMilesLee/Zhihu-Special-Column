34b27cf6374a2d3e68022561d9a099f43049b466
TheRealMilesLee <1789216340lhy@gmail.com>
2020-08-11 18:26:15 +0800

    update

---
+++ C++/Destructor/ContactInfo.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Miles on 2020/8/9.
+//
+
+#include "ContactInfo.h"
---
+++ C++/Destructor/cmake-build-debug/CMakeFiles/Destructor.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Destructor.dir/ContactInfo.cpp.o"
+  "CMakeFiles/Destructor.dir/main.cpp.o"
+  "Destructor"
+  "Destructor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Destructor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
